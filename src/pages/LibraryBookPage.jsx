d6c0f3f259a87f606a19b630b21225198f23f20e
jennycherny <evchern.it@gmail.com>
2024-01-31 16:25:27 +0700

    fixed bugs

--- src/pages/LibraryBookPage.jsx
+++ src/pages/LibraryBookPage.jsx
@@ -15,7 +15,6 @@ const LibraryBookPage = () => {
     const foundBook = data.find(book => book.id === numericBookId);
 
     const [detailsOpen, setDetailsOpen] = useState(false);
-    const [isModalVisible, setModalVisible] = useState(false);
 
     const isBookInRentCart = foundBook && rentCart.some((cartBook) => cartBook.id === foundBook.id);
     const isBookInBuyCart = foundBook && buyCart.some((cartBook) => cartBook.id === foundBook.id);
@@ -43,14 +42,6 @@ const LibraryBookPage = () => {
     const handleToggleDetails = () => {
       setDetailsOpen(!detailsOpen);
     };
-
-    const handleMouseEnter = () => {
-        setModalVisible(true);
-      };
-    
-      const handleMouseLeave = () => {
-        setModalVisible(false);
-      };
 
     if (isLoading) {
         return (
@@ -83,14 +74,8 @@ const LibraryBookPage = () => {
                         <h3>{foundBook.author}</h3>
                     </div>
 
-                    <div className="bookpage-price svg-container" id="svg-container" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                    <div className="bookpage-price">
                         <h2> {foundBook.rentPrice} GEL/ {foundBook.price} GEL</h2>
-                        <svg width='20px' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
-                        </svg>
-                        <div id="modal-library-book-page">
-                            Первая цена -<br />за аренду, а вторая<br />- за покупку
-                        </div>
                     </div>
 
                     <div className="bookpage-img">
@@ -125,13 +110,13 @@ const LibraryBookPage = () => {
                             <div className="bookpage-desc-buttons">
                             <button className={rentCartButtonClass} onClick={handleAddToRentCart}>
                                 <svg width="18px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
                                 </svg>
                                 <span>{rentCartButtonText}</span>
                             </button>
                             <button className={buyCartButtonClass} onClick={handleAddToBuyCart}>
                                 <svg width="18px" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
                                 </svg>
                                 <span>{buyCartButtonText}</span>
                             </button>
