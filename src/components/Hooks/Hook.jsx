44524588839393ecbc1bfdd2dfe4eb8ce45c2951
jennycherny <evchern.it@gmail.com>
2024-02-14 10:58:58 +0400

    added a gallery of miniatures for gifts

--- src/components/Hooks/Hook.jsx
+++ src/components/Hooks/Hook.jsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
@@ -7,81 +7,101 @@ const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Hook = () => {
-    const [data, setData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const { data: booksData, error: booksError } = await supabase
-                    .from('BooksData')
-                    .select('*');
-                
-                const { data: giftsData, error: giftsError } = await supabase
-                    .from('GiftsData')
-                    .select('*');
-                console.log(giftsData);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: booksData, error: booksError } = await supabase
+          .from("BooksData")
+          .select("*");
 
-                const { data: promoData, error: promoError } = await supabase
-                    .from('Promo')
-                    .select('id, promo');
+        const { data: giftsData, error: giftsError } = await supabase
+          .from("GiftsData")
+          .select("*");
+        console.log(giftsData);
 
-                const { data: imagesData, error: imagesError } = await supabase
-                    .from('ImagesData')
-                    .select('id, img2, img3, img4');
+        const { data: promoData, error: promoError } = await supabase
+          .from("Promo")
+          .select("id, promo");
 
-                const { data: languageData, error: languageError } = await supabase
-                    .from('Language')
-                    .select('id, language');
-                
-                const { data: rentedData, error: rentedError } = await supabase
-                    .from('Rented')
-                    .select('id, rented');
-                
-                if (booksError || giftsError || promoError || imagesError || languageError || rentedError) {
-                    setError(booksError || giftsError || promoError || imagesError || languageError || rentedError);
-                } else {
-                    const processedBooksData = booksData.map(book => {
-                        const promo = promoData.find(promo => promo.id === book.id);
-                        const images = imagesData.find(image => image.id === book.id);
-                        const language = languageData.find(language => language.id === book.id);
-                        const rented = rentedData.find(rented => rented.id === book.id);
+        const { data: imagesData, error: imagesError } = await supabase
+          .from("ImagesData")
+          .select("id, img2, img3, img4");
+        console.log(imagesData);
 
-                        return {
-                            ...book,
-                            promo: promo ? promo.promo : null,
-                            images: images ? images : null,
-                            language: language ? language.language : null,
-                            rented: rented ? rented.rented : null,
-                        };
-                    });
+        const { data: languageData, error: languageError } = await supabase
+          .from("Language")
+          .select("id, language");
 
+        const { data: rentedData, error: rentedError } = await supabase
+          .from("Rented")
+          .select("id, rented");
 
-                    const processedGiftsData = giftsData.map(gift => {
-                        return gift;
-                    });
+        if (
+          booksError ||
+          giftsError ||
+          promoError ||
+          imagesError ||
+          languageError ||
+          rentedError
+        ) {
+          setError(
+            booksError ||
+              giftsError ||
+              promoError ||
+              imagesError ||
+              languageError ||
+              rentedError
+          );
+        } else {
+          const processedBooksData = booksData.map((book) => {
+            const promo = promoData.find((promo) => promo.id === book.id);
+            const images = imagesData.find((image) => image.id === book.id);
+            const language = languageData.find(
+              (language) => language.id === book.id
+            );
+            const rented = rentedData.find((rented) => rented.id === book.id);
 
-                    const combinedData = [...processedBooksData, ...processedGiftsData];
-                    
-                    setData(combinedData);
-                }
-            } catch (error) {
-                console.error('Ошибка при запросе данных:', error);
-                setError(error);
-            } finally {
-                setIsLoading(false);
-            }
-        };
+            return {
+              ...book,
+              promo: promo ? promo.promo : null,
+              images: images ? images : null,
+              language: language ? language.language : null,
+              rented: rented ? rented.rented : null,
+            };
+          });
 
-        fetchData();
-    }, []);
+          const processedGiftsData = giftsData.map((gift) => {
+            const images = imagesData.find((image) => image.id === gift.id);
 
-    return {
-        data,
-        isLoading,
-        error,
+            return {
+              ...gift,
+              images: images ? images : null,
+            };
+          });
+
+          const combinedData = [...processedBooksData, ...processedGiftsData];
+          setData(combinedData);
+        }
+      } catch (error) {
+        console.error("Ошибка при запросе данных:", error);
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
     };
+
+    fetchData();
+  }, []);
+
+  return {
+    data,
+    isLoading,
+    error,
+  };
 };
 
 export default Hook;
--- src/pages/ShopBookPage.jsx
+++ src/pages/ShopBookPage.jsx
@@ -1,164 +1,204 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router';
-import Hook from '../components/Hooks/Hook';
-import { useCart } from '../components/Providers/CartProvider';
+import React, { useState } from "react";
+import { useParams } from "react-router";
+import Hook from "../components/Hooks/Hook";
+import { useCart } from "../components/Providers/CartProvider";
 
-import './css/ShopBookPage.css'
+import "./css/ShopBookPage.css";
 import { MdOutlineExpandMore, MdOutlineExpandLess } from "react-icons/md";
-import Footer from '../components/Footer/Footer';
+import Footer from "../components/Footer/Footer";
 
 const ShopBookPage = () => {
-    const { data, isLoading } = Hook();
-    const { bookId } = useParams();
-    const { addToBuyCart, removeFromBuyCart, buyCart } = useCart();
-    const numericBookId = parseInt(bookId, 10);
+  const { data, isLoading } = Hook();
+  const { bookId } = useParams();
+  const { addToBuyCart, removeFromBuyCart, buyCart } = useCart();
+  const numericBookId = parseInt(bookId, 10);
 
-    const foundBook = data.find(book => book.id === numericBookId);
-    console.log(foundBook)
-    const [detailsOpen, setDetailsOpen] = useState(false);
-    const [currentImage, setCurrentImage] = useState(foundBook && foundBook.img ? foundBook.img : '');
-    
-    const isBookInCart = foundBook && buyCart.some((cartBook) => cartBook.id === foundBook.id);
-    const cartButtonText = isBookInCart ? 'Добавлено' : 'В корзину';
-    const cartButtonClass = `add-to-cart ${isBookInCart ? 'added' : ''}`;
+  const foundBook = data.find((book) => book.id === numericBookId);
+  console.log(foundBook);
+  const [detailsOpen, setDetailsOpen] = useState(false);
+  const [currentImage, setCurrentImage] = useState(
+    foundBook && foundBook.img ? foundBook.img : ""
+  );
 
-    const handleCartAction = () => {
-        if (isBookInCart) {
-            removeFromBuyCart(foundBook);
-        } else {
-            addToBuyCart(foundBook);
-        }
-    };
+  const isBookInCart = foundBook && buyCart.some((cartBook) => cartBook.id === foundBook.id);
+  const cartButtonText = isBookInCart ? "Добавлено" : "В корзину";
+  const cartButtonClass = `add-to-cart ${isBookInCart ? "added" : ""}`;
 
-    const handleToggleDetails = () => {
-      setDetailsOpen(!detailsOpen);
-    };
-
-    if (isLoading) {
-        return (
-            <div>
-                <p></p>
-            </div>
-        );
+  const handleCartAction = () => {
+    if (isBookInCart) {
+      removeFromBuyCart(foundBook);
+    } else {
+      addToBuyCart(foundBook);
     }
+  };
 
-    if (!foundBook || foundBook.condition === 'Б/У') {
-        return (
-            <div className='bnf-container'>
-                <p>Книга не найдена 😥</p>
-            </div>
-        );
-    }
+  const handleToggleDetails = () => {
+    setDetailsOpen(!detailsOpen);
+  };
 
-    const descParagraphs = (text) => {
-        return text.split('\n').map((paragraph, index) => (
-          <p key={index}>{paragraph}</p>
-        ));
-      };
-    
-    const handleThumbnailClick = (img) => {
-        setCurrentImage(img);
-    };
-
+  if (isLoading) {
     return (
-        <div>
-            <div className='bookpage'>
-                <div className='bookpage-container'>
-                    <div className="bookpage-header">
-                        <h2>{foundBook.title}</h2>
-                        <h3>{foundBook.author}</h3>
-                    </div>
-
-                    <div className="bookpage-price">
-                        <h2>{foundBook.price} GEL</h2>
-                    </div>
-
-                    {foundBook && (
-                        <div className='bookpage-img'>
-                            {foundBook.img && (
-                                <img src={currentImage || foundBook.img} alt="Фото книги" />
-                            )}
-                            {foundBook.images && (
-                                <div className='thumbnails-container'>
-                                    <div className='bookpage-img-thumbnail' onClick={() => handleThumbnailClick(foundBook.img)}>
-                                        <img src={foundBook.img} alt='Thumbnail 1' />
-                                    </div>
-                                    {foundBook.images.img2 && (
-                                        <div className='bookpage-img-thumbnail' onClick={() => handleThumbnailClick(foundBook.images.img2)}>
-                                            <img src={foundBook.images.img2} alt='Thumbnail 2' />
-                                        </div>
-                                    )}
-                                    {foundBook.images.img3 && (
-                                        <div className='bookpage-img-thumbnail' onClick={() => handleThumbnailClick(foundBook.images.img3)}>
-                                            <img src={foundBook.images.img3} alt='Thumbnail 3' />
-                                        </div>
-                                    )}
-                                    {foundBook.images.img4 && (
-                                        <div className='bookpage-img-thumbnail' onClick={() => handleThumbnailClick(foundBook.images.img4)}>
-                                            <img src={foundBook.images.img4} alt='Thumbnail 4' />
-                                        </div>
-                                    )}
-                                </div>
-                            )}
-
-                            {foundBook && foundBook.condition !== 'Подарки' && (
-                                <div className="bookpage-data">
-                                    <div onClick={handleToggleDetails} className="bookpage-data-more"> 
-                                        <span>Подробнее о книге</span>
-                                        {detailsOpen ? (
-                                            <MdOutlineExpandLess color='#6b6869' size="22px" className="bookpage-data-icon" />
-                                        ) : (
-                                            <MdOutlineExpandMore color='#6b6869' size="22px" className="bookpage-data-icon" />
-                                        )}
-                                    </div>
-
-                                    {detailsOpen && (
-                                        <div className={`book-details-expanded ${detailsOpen ? 'open' : ''}`}>
-                                            <h7 className="category">Категория: {foundBook.category}</h7>
-                                            <h7 className="publishing">Издательство: {foundBook.publishing}</h7>
-                                            <h7 className="age">Возраст: {foundBook.age}+</h7>
-                                            <h7 className="cover">Обложка: {foundBook.cover}</h7>
-                                            <h7 className="condition">Состояние: {foundBook.condition}</h7>
-                                        </div>
-                                    )}
-                                </div>
-                            )}
-                        </div>
-                    )}
-
-                    <div className="bookpage-desc">
-                        {foundBook.stock === 0 ? (
-                            <p className="out-of-stock-message">Нет в наличии</p>
-                            ) : (
-                            <button className={cartButtonClass} onClick={handleCartAction}>
-                                <svg
-                                width="18px"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                                >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                                />
-                                </svg>
-                                <span>{cartButtonText}</span>
-                            </button>
-                            )}
-                        <div className="bookpage-desc-text">
-                            {descParagraphs(foundBook.desc)}
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <Footer/>
-        </div>
+      <div>
+        <p></p>
+      </div>
     );
+  }
+
+  if (!foundBook || foundBook.condition === "Б/У") {
+    return (
+      <div className="bnf-container">
+        <p>Книга не найдена 😥</p>
+      </div>
+    );
+  }
+
+  const descParagraphs = (text) => {
+    return text
+      .split("\n")
+      .map((paragraph, index) => <p key={index}>{paragraph}</p>);
+  };
+
+  const handleThumbnailClick = (img) => {
+    setCurrentImage(img);
+  };
+
+  return (
+    <div>
+      <div className="bookpage">
+        <div className="bookpage-container">
+          <div className="bookpage-header">
+            <h2>{foundBook.title}</h2>
+            <h3>{foundBook.author}</h3>
+          </div>
+
+          <div className="bookpage-price">
+            <h2>{foundBook.price} GEL</h2>
+          </div>
+
+          {foundBook && (
+            <div className="bookpage-img">
+              {foundBook.img && (
+                <img src={currentImage || foundBook.img} alt="Фото книги" />
+              )}
+              {foundBook.images && (
+                <div className="thumbnails-container">
+                  <div
+                    className="bookpage-img-thumbnail"
+                    onClick={() => handleThumbnailClick(foundBook.img)}
+                  >
+                    <img src={foundBook.img} alt="Thumbnail 1" />
+                  </div>
+                  {foundBook.images.img2 && (
+                    <div
+                      className="bookpage-img-thumbnail"
+                      onClick={() =>
+                        handleThumbnailClick(foundBook.images.img2)
+                      }
+                    >
+                      <img src={foundBook.images.img2} alt="Thumbnail 2" />
+                    </div>
+                  )}
+                  {foundBook.images.img3 && (
+                    <div
+                      className="bookpage-img-thumbnail"
+                      onClick={() =>
+                        handleThumbnailClick(foundBook.images.img3)
+                      }
+                    >
+                      <img src={foundBook.images.img3} alt="Thumbnail 3" />
+                    </div>
+                  )}
+                  {foundBook.images.img4 && (
+                    <div
+                      className="bookpage-img-thumbnail"
+                      onClick={() =>
+                        handleThumbnailClick(foundBook.images.img4)
+                      }
+                    >
+                      <img src={foundBook.images.img4} alt="Thumbnail 4" />
+                    </div>
+                  )}
+                </div>
+              )}
+
+              {foundBook && foundBook.condition !== "Подарки" && (
+                <div className="bookpage-data">
+                  <div
+                    onClick={handleToggleDetails}
+                    className="bookpage-data-more"
+                  >
+                    <span>Подробнее о книге</span>
+                    {detailsOpen ? (
+                      <MdOutlineExpandLess
+                        color="#6b6869"
+                        size="22px"
+                        className="bookpage-data-icon"
+                      />
+                    ) : (
+                      <MdOutlineExpandMore
+                        color="#6b6869"
+                        size="22px"
+                        className="bookpage-data-icon"
+                      />
+                    )}
+                  </div>
+
+                  {detailsOpen && (
+                    <div
+                      className={`book-details-expanded ${
+                        detailsOpen ? "open" : ""
+                      }`}
+                    >
+                      <h7 className="category">
+                        Категория: {foundBook.category}
+                      </h7>
+                      <h7 className="publishing">
+                        Издательство: {foundBook.publishing}
+                      </h7>
+                      <h7 className="age">Возраст: {foundBook.age}+</h7>
+                      <h7 className="cover">Обложка: {foundBook.cover}</h7>
+                      <h7 className="condition">
+                        Состояние: {foundBook.condition}
+                      </h7>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+          )}
+
+          <div className="bookpage-desc">
+            {foundBook.stock === 0 ? (
+              <p className="out-of-stock-message">Нет в наличии</p>
+            ) : (
+              <button className={cartButtonClass} onClick={handleCartAction}>
+                <svg
+                  width="18px"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+                  />
+                </svg>
+                <span>{cartButtonText}</span>
+              </button>
+            )}
+            <div className="bookpage-desc-text">
+              {descParagraphs(foundBook.desc)}
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default ShopBookPage;
